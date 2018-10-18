ff650316b04fe189d4fcfa489a943eb8495f2fca
phytomosaic <smithr2@oregonstate.edu>
2018-10-18 15:38:01 -0700

    allow abundance-weighted density in `plot_joy`

--- R/plot_joy.R
+++ R/plot_joy.R
@@ -5,7 +5,15 @@
 #'     also known as a joy plot or stacked distributions.
 #'
 #' @param m  matrix or data.frame, where each column vector will have
-#' its own density polygon
+#'      its own density polygon
+#'
+#' @param w matrix or data.frame, of same dimensions as \code{m}, with
+#'      non-negative weights (in ecology, this might be species
+#'      abundances)
+#'
+#' @param scaled logical, should polygons be scaled to maximum?
+#'
+#' @param excl   numeric, value(s) to exclude from density estimate
 #'
 #' @param fcol   polygon fill color(s)
 #'
@@ -23,13 +31,11 @@
 #'
 #' @param ypad   vertical padding for entire plot
 #'
-#' @param scaled logical, should polygons be scaled to maximum?
-#'
 #' @param pts    optional vector of points to plot onto each row
 #'
 #' @param labcex expansion factor for y-axis text labels
 #'
-#' @param ...    further arguments passed to other methods
+#' @param ...    further arguments passed to plot function
 #'
 #' @return
 #' None, plots to device.
@@ -43,68 +49,82 @@
 #' overlap.
 #'
 #' @examples
+#' # synthetic example
 #' set.seed(23)
-#' N <- 99
-#' x <- rnorm(N,10,1)
-#' y <- x^1.2 + rnorm(N,0,1)
-#' z <- x^1.4 + rnorm(N,0,1)
-#' m <- data.frame(x,y,z)
-#' # allow overlap
-#' plot_joy(m, ypad=1.05, labcex=1, xlab='Gradient X')
-#' # forbid overlap
-#' plot_joy(m, yexp = 1, ypad=1, labcex=1, xlab='Gradient X')
-#' # color vector
-#' plot_joy(m, fcol=c("#FF000080", "#A020F080", "#0000FF80"),
-#'          yexp = 1, ypad=1, labcex=1, xlab='Gradient X')
+#' x <- rnorm(99,10,1)
+#' m <- data.frame(x = x,
+#'                 y = x^1.2 + rnorm(99,0,1),
+#'                 z = x^1.4 + rnorm(99,0,1))
+#' plot_joy(m, ypad=1.05, labcex=1)        # allow overlap
+#' plot_joy(m, yexp=1, ypad=1, labcex=1)   # forbid overlap
+#' plot_joy(m, yexp=1, ypad=1, labcex=1, fcol=4:6) # color vector
+#'
+#' # real example
+#' data(braun)
+#' abu <- braun$spe              # species abundances
+#' abu <- abu[colMeans(abu)>1]   # keep only most dominant species
+#' env <- braun$env$bio1         # environment
+#' occ <- abu
+#' occ[occ>0] <- 1               # binary occurrences matrix
+#' y   <- occ*env                # matrix of env values at occurrences
+#' lab <- ' Annual Mean Temperature'
+#' set_par(2)
+#' plot_joy(y, w=NULL, xlab=lab) # unweighted
+#' plot_joy(y, w=abu,  xlab=lab) # abundance-weighted
 #'
 #' @export
 #' @rdname plot_joy
-`plot_joy` <- function(
-     m,                  # matrix where each column will have its own density polygon
-     fcol = rgb(0,0,0,0.5), # polygon fill color
-     lcol = 'black',     # polygon line color
-     lwid = 1,           # polygon line width
-     xinc = 0,           # horiz offset for ea polygon (0 = aligned)
-     xexp = 0.2,         # x-axis expansion factor for entire plot
-     yinc = ncol(m),     # vert offset for ea polygon
-     yexp = 1.2,         # vertical expansion factor for ea polygon
-     ypad = 1,           # vertical padding for entire plot
-     scaled = TRUE,      # should polygons be scaled to maximum?
-     pts=NULL,
-     labcex=0.5,
-     ...){
-     opar <- par()
-     on.exit(par(opar))
+`plot_joy` <- function (m, w=NULL, scaled = TRUE, excl = 0,
+                        fcol = '#00000080', lcol = '#000000',
+                        lwid = 1, xinc = 0, xexp = 0.2,
+                        yinc = ncol(m), yexp = 1.2, ypad = 1,
+                        pts = NULL, labcex = 0.5, ...) {
+     hasw <- !is.null(w)
+     if (hasw){
+          w <- as.matrix(w)
+          if (!identical(dim(m), dim(w))) {
+               stop('weights and data of unequal dimensions')
+          }
+     }
+     m    <- as.matrix(m)
+     nm   <- dimnames(m)[[2]]
      nspp <- dim(m)[2]
-     xrng <- range(m, na.rm=T)
-     xmin <- xrng[1]-(diff(xrng)*xexp)
-     xmax <- xrng[2]+(diff(xrng)*xexp)
-     ht   <- nspp*ypad
-     if(length(fcol)==1) fcol <- rep(fcol, nspp)
-     par( mfrow=c(1,1), las=1 )
-     plot(1, xlim=c(xmin, xmax), ylim=c(0,ht),
-          type='n', xaxs='i', yaxs='i', bty='l',
-          yaxt='n', ylab='', las=1, ...)
-     for(i in nspp:1){
-          x  <- na.omit(m[,i])
-          if(length(unique(x))==1){
-               fuzz <- diff(xrng)*0.01
-               x <- c(x-fuzz, x,x,x, x+fuzz)
+     xrng <- range(m[!(m %in% excl)], na.rm = T)
+     xmin <- xrng[1] - (diff(xrng) * xexp)
+     xmax <- xrng[2] + (diff(xrng) * xexp)
+     ht   <- nspp * ypad
+     if (length(fcol) == 1)
+          fcol <- rep(fcol, nspp)
+     plot(1, xlim = c(xmin, xmax), ylim = c(0, ht), type = 'n',
+          xaxs = 'i', yaxs = 'i', bty = 'l', yaxt = 'n', ylab = '',
+          ...)
+     for (j in nspp:1) {
+          x  <- m[, j]
+          if (hasw){
+               wx <- w[, j][!(x %in% excl)]
+               wx <- wx/sum(wx)
+          } else {
+               wx <- NULL
           }
-          d <- stats::density(x, na.rm=T)
-          if(scaled){
+          x  <-  x[!(x %in% excl)]
+          if (length(unique(x)) == 1) {
+               fuzz <- diff(xrng) * 0.01
+               x  <- c(x - fuzz, x, x, x, x + fuzz)
+               wx <- NULL
+          }
+          d <- stats::density(x, weights = wx)
+          if (scaled)
                d$y <- d$y/(max(d$y))
+          gx <- (j - 1) * (max(x) - min(x)) * xinc
+          gy <- (j - 1) * (1/ht) * yinc
+          zero <- min(yexp * d$y + gy)
+          polygon(d$x + gx, yexp * d$y + gy, col = fcol[j])
+          lines(d$x + gx, yexp * d$y + gy, col = lcol, lwd = lwid)
+          lines(c(xmin, xmax), rep(zero, 2))
+          if (!is.null(pts)) {
+               points(pts[j], zero, pch = 16, col = 'black', cex=0.5)
           }
-          gx <- (i-1)*(max(x)-min(x))*xinc     # horizontal offset
-          gy <- (i-1)*(1/ht)*yinc              # vertical offset
-          polygon(d$x+gx, yexp*d$y+gy, col=fcol[i])
-          lines(  d$x+gx, yexp*d$y+gy, col=lcol, lwd=lwid)
-          lines(c(xmin, xmax), rep(min(yexp*d$y+gy),2)  )
-          if(!is.null(pts)){
-               points(pts[i], min(yexp*d$y+gy),
-                      pch=16, col='black', cex=0.5)
-          }
-          mtext(dimnames(m)[[2]][i], side=2, line=0.5, outer=F,
-                at=min(yexp*d$y+gy), padj=0, cex=labcex)
+          mtext(nm[j], side = 2, line = 0.5, outer = F, at = zero,
+                padj = 0, cex = labcex, las=1)
      }
 }
