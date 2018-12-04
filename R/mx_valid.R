80ff12f3a9193c155ebccf76be3e3cd34580a5cd
phytomosaic <smithr2@oregonstate.edu>
2018-12-04 10:34:00 -0800

    Option to avoid vegan::distconnected for speed

--- R/mx_valid.R
+++ R/mx_valid.R
@@ -5,6 +5,10 @@
 #'
 #' @param x array of species data, where rows = SUs and cols = species
 #'
+#' @param checkconnect logical, check whether data are connected,
+#'     using \code{\link[vegan]{distconnected}}?  Default \code{FALSE}
+#'     saves time and memory.
+#'
 #' @param ... further arguments passed to other methods
 #'
 #' @return
@@ -13,8 +17,10 @@
 #' @details
 #' Often useful before further analysis such as calculating certain
 #' dissimilarity measures or during rejection sampling of simulated
-#' data.  Data are strictly 'connected' when all sample units share
-#' the same species pool (no disconnected species pools).
+#' data.  Data are strictly `connected` when all sample units share
+#' the same species pool (no disconnected species pools), checked
+#' using \code{\link[vegan]{distconnected}} and Bray-Curtis
+#' dissimilarities.
 #'
 #' @examples
 #' # species abundance data
@@ -30,11 +36,15 @@
 #'
 #' @export
 #' @rdname mx_valid
-`mx_valid` <- function(x, ...){
+`mx_valid` <- function(x, checkconnect=FALSE, ...){
      lacksna <- !anyNA(x)
      allrow  <- all(rowSums(x, na.rm=T)!=0)
      allcol  <- all(colSums(x, na.rm=T)!=0)
-     isconnected <- sum(unique(vegan::distconnected(
-          suppressWarnings(vegan::vegdist(x)),1,F)))==1
+     if (checkconnect){
+          isconnected <- sum(unique(vegan::distconnected(
+               suppressWarnings(vegan::vegdist(x)),1,F)))==1
+     } else {
+          isconnected <- TRUE
+     }
      return(lacksna & allrow & allcol & isconnected)
 }
