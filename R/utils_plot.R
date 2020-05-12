03e5ea9cfeebfbc4b6ee8e9a8f9871a2a13f8179
phytomosaic <smithr2@oregonstate.edu>
2020-05-12 07:55:01 -0700

    Simply revert to `set_par` if `set_par_mercury` can't access custom fonts

--- R/utils_plot.R
+++ R/utils_plot.R
@@ -8,8 +8,6 @@
 #'
 #' @param CEX character expansion factor (default = 1).
 #'
-#' @param char alphabetic character to add as panel label.
-#'
 #' @param x numeric, \emph{relative} horizontal position
 #'
 #' @param y numeric, \emph{relative} vertical position
@@ -20,8 +18,8 @@
 #'
 #' @return
 #' `set_par` sets graphical parameters, and `set_par_mercury` does so
-#'     in a retro style; `add_label` adds a panel label to plot
-#'     margins; `add_text` adds text to the plotting area.
+#'     in a retro style; `add_label` adds a panel label to upper
+#'     margin; `add_text` adds text to the plotting area.
 #'
 #' @examples
 #' ### standard usage
@@ -38,6 +36,7 @@
 #' op <- set_par_mercury()
 #' plot(x, y, cex=0.7, asp=0.25, ylab='f(x)')
 #' add_text(.1, .9, 'SINUSOIDAL', cex=0.9)
+#' add_label('A')
 #' par(op)
 #'
 #' ### Project Mercury space missions
@@ -63,184 +62,103 @@
 #'      ylab='Duration weightless (h)', xlab='Date', cex=0.7)
 #' mtext('Project Mercury missions', side=3, line=2, cex=1.5, at=-3900)
 #' par(op)
-
-
-
-# `set_par` <- function (panels = 1, pty = 's', ...) {
-#         mgp <- c(2, 0.4, 0)
-#         mar <- c(4, 4, 0.5, 0.5)
-#         oma <- c(0, 0, 0, 0)
-#         `auto_rowcol` <- function(n = panels) {
-#                 if (n <= 3)
-#                         c(1, n)
-#                 else if (n <= 6)
-#                         c(2, (n + 1)%/%2)
-#                 else if (n <= 12)
-#                         c(3, (n + 2)%/%3)
-#                 else c(ceiling(n/(nr <- ceiling(sqrt(n)))), nr)
-#         }
-#         mfrow <- auto_rowcol()
-#         if(panels > 4) panels <- 4
-#         switch(as.character(panels),
-#                `4` = par(mfrow = mfrow, mgp = mgp, mar = mar, pty = pty,
-#                          oma = oma, bty = 'L', las = 1, cex.lab = 1.2,
-#                          tcl=-0.2, ...),
-#                `3` = par(mfrow = mfrow, mgp = mgp, mar = mar, pty = pty,
-#                          oma = oma, bty = 'L', las = 1, cex.lab = 1.4,
-#                          cex.axis = 1.2, tcl=-0.2, ...),
-#                `2` = par(mfrow = mfrow, mgp = mgp, mar = mar, pty = pty,
-#                          oma = oma, bty = 'L', las = 1, cex.axis = 0.85,
-#                          tcl=-0.2, ...),
-#                `1` = par(mfrow = mfrow, mgp = mgp, mar = mar, pty = pty,
-#                          oma = oma, bty = 'L', las = 1, cex.axis = 0.85,
-#                          tcl=-0.2, ...))
-# }
-
-
+#'
+#'
 #' @export
 #' @rdname utils_plot
 `set_par` <- function (panels = NULL, CEX=1.0, ...) {
-        z  <- list(...)
-        nm <- names(z)  # NULL if no additional arguments given
-        `f` <- function(x, val) { if (!(x %in% nm)) z[[x]] <<- val }
-        f('pch', 16)
-        f('mar', c(4, 4, 1, 0.5))
-        f('oma', c(0.1, 0.1, 0.1, 0.1))
-        f('bty', 'L')
-        f('pty', 's')
-        f('las', 1)
-        f('mgp', c(2, 0.4, 0))
-        f('tcl', -0.2)
-        if(!is.null(panels)) {
-                `auto_rowcol` <- function(n = panels) {
-                        if (n <= 3)
-                                c(1, n)
-                        else if (n <= 6)
-                                c(2, (n + 1)%/%2)
-                        else if (n <= 12)
-                                c(3, (n + 2)%/%3)
-                        else c(ceiling(n/(nr <- ceiling(sqrt(n)))), nr)
-                }
-                mfrow <- auto_rowcol()
-                f('mfrow', mfrow)
-                if (panels > 4) panels <- 4
-                switch(as.character(panels),
-                       `4` = f('cex.lab', 1.2),
-                       `3` = { f('cex.axis', 1.2); f('cex.lab', 1.4) },
-                       `2` = f('cex.axis', 0.85),
-                       `1` = f('cex.axis', 0.85))
-        }
-        if (length(names(z)) > 1) z <- z[order(names(z))]
-        invisible(par(z)) # set parameters, while keeping defaults
+     z  <- list(...)
+     nm <- names(z)  # NULL if no additional arguments given
+     `f` <- function(x, val) { if (!(x %in% nm)) z[[x]] <<- val }
+     f('pch', 16)
+     f('mar', c(4, 4, 1, 0.5))
+     f('oma', c(0.1, 0.1, 0.1, 0.1))
+     f('bty', 'L')
+     f('pty', 's')
+     f('las', 1)
+     f('mgp', c(2, 0.4, 0))
+     f('tcl', -0.2)
+     if(!is.null(panels)) {
+          `auto_rowcol` <- function(n = panels) {
+               if (n <= 3)
+                    c(1, n)
+               else if (n <= 6)
+                    c(2, (n + 1)%/%2)
+               else if (n <= 12)
+                    c(3, (n + 2)%/%3)
+               else c(ceiling(n/(nr <- ceiling(sqrt(n)))), nr)
+          }
+          mfrow <- auto_rowcol()
+          f('mfrow', mfrow)
+          if (panels > 4) panels <- 4
+          switch(as.character(panels),
+                 `4` = f('cex.lab', 1.2),
+                 `3` = { f('cex.axis', 1.2); f('cex.lab', 1.4) },
+                 `2` = f('cex.axis', 0.85),
+                 `1` = f('cex.axis', 0.85))
+     }
+     if (length(names(z)) > 1) z <- z[order(names(z))]
+     invisible(par(z)) # set parameters, while keeping defaults
 }
-
 #' @export
 #' @rdname utils_plot
 `set_par_mercury` <- function(panels=NULL, CEX=1.0, ...) {
-        `check_has_font` <- function() {   ###   unexported   ###
-                if(Sys.info()[1] != 'Windows')
-                        stop('Currently only supported on windows devices')
-                ### import, install and register the font
-                ###   from: https://webonastick.com/fonts/routed-gothic/
-                require(extrafont)
-                has <- 'Routed Gothic' %in% extrafont::fonttable()$FamilyName
-                if (!has) {
-                        message('Downloading `Routed Gothic` font now...\n')
-                        ### 1. download the font if it doesnt exist
-                        g <- 'https://github.com/dse/routed-gothic/blob/master/'
-                        g <- paste0(g, 'dist/ttf/')
-                        f1 <- tempfile(pattern = 'font_1_to_install__')
-                        download.file(url=paste0(g,'routed-gothic.ttf'),
-                                      destfile=f1)
-                        f2 <- tempfile(pattern = 'font_2_to_install__')
-                        download.file(url=paste0(g,'routed-gothic-italic.ttf'),
-                                      destfile=f2)
-                        msg <- '\nNow manually install these two fonts'
-                        msg <- paste0(msg,'on your system:\n')
-                        message(paste0(msg, f1, '\nand\n', f2, '\n'))
-                        opendir <- function(dir = tempdir()){
-                                if (.Platform['OS.type'] == 'windows'){
-                                        shell.exec(dir)
-                                } else {
-                                        system(paste(Sys.getenv('R_BROWSER'), dir))
-                                }
-                        }
-                        opendir()
-                        ### 2. pause to manually install
-                        msg <- paste0('\n\nHave you installed these files?')
-                        msg <- paste0(msg, '\n\nPress y or n to continue...\n\n')
-                        resp <- readline(paste0(msg))
-                        if (!grepl('y', resp, ignore.case=T)) {
-                                message('Must manually install fonts on system!')
-                                return(invisible())
-                        } else {
-                                ### 3. import it
-                                extrafont::font_import(pattern='routed-gothic',
-                                                       prompt=F)
-                                ### 4. register it for multiple devices
-                                extrafont::loadfonts(device='win')
-                                extrafont::loadfonts(device='pdf')
-                                extrafont::loadfonts(device='postscript')
-                                ### 5. translate to windows and pdf devices
-                                windowsFonts(sans='Routed Gothic')
-                                pdfFonts(sans='Routed Gothic')
-                        }
-                        message('Fonts successfully loaded; more details at:')
-                        message(paste0('https://cran.r-project.org/web/',
-                                       'packages/extrafont/README.html'))
-                        return(TRUE)
-                } else {
-                        return(TRUE)
-                }
-        }
-        stopifnot(isTRUE(check_has_font()))
-        z  <- list(...)
-        nm <- names(z)  # NULL if no additional arguments given
-        `f` <- function(x, val) { if (!(x %in% nm)) z[[x]] <<- val }
-        f('cex.axis', CEX*1)
-        f('cex.lab',  CEX*1.3)
-        f('cex.main', CEX*1.3)
-        f('pch', 16)
-        f('mar', c(3, 3, 1, 0.5))
-        f('oma', c(0.1, 0.1, 0.1, 0.1))
-        f('family', 'Routed Gothic')
-        f('bty', 'L')
-        f('pty', 's')
-        f('las', 1)
-        f('mgp', c(CEX+0.5, 0.2, 0))
-        f('tcl', 0.2)
-        if(!is.null(panels)) {
-                `auto_rowcol` <- function(n = panels) {
-                        if (n <= 3)
-                                c(1, n)
-                        else if (n <= 6)
-                                c(2, (n + 1)%/%2)
-                        else if (n <= 12)
-                                c(3, (n + 2)%/%3)
-                        else c(ceiling(n/(nr <- ceiling(sqrt(n)))), nr)
-                }
-                f('mfrow', auto_rowcol())
-                if (panels > 4) panels <- 4
-                switch(as.character(panels),
-                       `4` = f('cex.lab',  CEX*1.4),
-                       `3` = { f('cex.axis', 1.0); f('cex.lab', 1.4) },
-                       `2` = f('cex.axis',  CEX*0.9),
-                       `1` = f('cex.axis',  CEX*0.9))
-        }
-        if (length(names(z)) > 1) z <- z[order(names(z))]
-        invisible(par(z)) # set parameters, while keeping defaults
+     # if font not installed, then revert to `set_par()`
+     require(extrafont)
+     if (!('Routed Gothic' %in% extrafont::fonttable()$FamilyName)) {
+          a <- c(as.list(environment()), list(...))
+          do.call(ecole::set_par, a)
+          warning('\nMust import/install/register the font from:\n
+               https://webonastick.com/fonts/routed-gothic/\n
+          ...trying `ecole::set_par()` instead...')
+     }
+     # if font is successfully installed, then proceed
+     z  <- list(...)
+     nm <- names(z)  # NULL if no additional arguments given
+     `f` <- function(x, val) { if (!(x %in% nm)) z[[x]] <<- val }
+     f('cex.axis', CEX*1)
+     f('cex.lab',  CEX*1.3)
+     f('cex.main', CEX*1.3)
+     f('pch', 16)
+     f('mar', c(3, 3, 1, 0.5))
+     f('oma', c(0.1, 0.1, 0.1, 0.1))
+     f('family', 'Routed Gothic')
+     f('bty', 'L')
+     f('pty', 's')
+     f('las', 1)
+     f('mgp', c(CEX+0.5, 0.2, 0))
+     f('tcl', 0.2)
+     if(!is.null(panels)) {
+          `auto_rowcol` <- function(n = panels) {
+               if (n <= 3)
+                    c(1, n)
+               else if (n <= 6)
+                    c(2, (n + 1)%/%2)
+               else if (n <= 12)
+                    c(3, (n + 2)%/%3)
+               else c(ceiling(n/(nr <- ceiling(sqrt(n)))),nr)
+          }
+          f('mfrow', auto_rowcol())
+          if (panels > 4) panels <- 4
+          switch(as.character(panels),
+                 `4` = f('cex.lab',  CEX*1.4),
+                 `3` = {f('cex.axis', 1.0); f('cex.lab', 1.4)},
+                 `2` = f('cex.axis',  CEX*0.9),
+                 `1` = f('cex.axis',  CEX*0.9))
+          # TODO: mgp should also vary with number of panels...
+     }
+     if (length(names(z)) > 1) z <- z[order(names(z))]
+     invisible(par(z)) # set parameters, while keeping defaults
 }
 #' @export
 #' @rdname utils_plot
-`add_label` <- function(char='a', ...){
-        if(!grepl('[[:alpha:]]',char)) stop('need alphabet char')
-        text <- paste(LETTERS[letters==char], ')', sep='')
-        mtext(text=text, side=3, adj=0, ...)
+`add_label` <- function(labels, ...){
+     mtext(text=labels, side=3, adj=0, ...)
 }
 #' @export
 #' @rdname utils_plot
 `add_text` <- function(x, y, labels, ...){
-        text(x=graphics::grconvertX(x, from='npc', to='user'),
-             y=graphics::grconvertY(y, from='npc', to='user'),
-             labels=labels, adj=0, ...)
+     text(x=graphics::grconvertX(x, from='npc', to='user'),
+          y=graphics::grconvertY(y, from='npc', to='user'),
+          labels=labels, adj=0, ...)
 }
