6d8b7d3f5221cd389027d27a06f91f88d0a4b2ac
phytomosaic <smithr2@oregonstate.edu>
2020-09-15 10:05:29 -0700

    Account for operating system

--- R/utils_plot.R
+++ R/utils_plot.R
@@ -63,98 +63,103 @@
 #' @export
 #' @rdname utils_plot
 `set_par` <- function (panels = NULL, CEX=1.0, ...) {
-     z  <- list(...)
-     nm <- names(z)  # NULL if no additional arguments given
-     `f` <- function(x, val) { if (!(x %in% nm)) z[[x]] <<- val }
-     f('pch', 16)
-     f('mar', c(4, 4, 1, 0.5))
-     f('oma', c(0.1, 0.1, 0.1, 0.1))
-     f('bty', 'L')
-     f('pty', 's')
-     f('las', 1)
-     f('mgp', c(2, 0.4, 0))
-     f('tcl', -0.2)
-     if(!is.null(panels)) {
-          `auto_rowcol` <- function(n = panels) {
-               if (n <= 3)
-                    c(1, n)
-               else if (n <= 6)
-                    c(2, (n + 1)%/%2)
-               else if (n <= 12)
-                    c(3, (n + 2)%/%3)
-               else c(ceiling(n/(nr <- ceiling(sqrt(n)))), nr)
-          }
-          mfrow <- auto_rowcol()
-          f('mfrow', mfrow)
-          if (panels > 4) panels <- 4
-          switch(as.character(panels),
-                 `4` = f('cex.lab', 1.2),
-                 `3` = { f('cex.axis', 1.2); f('cex.lab', 1.4) },
-                 `2` = f('cex.axis', 0.85),
-                 `1` = f('cex.axis', 0.85))
-     }
-     if (length(names(z)) > 1) z <- z[order(names(z))]
-     invisible(par(z)) # set parameters, while keeping defaults
+        z  <- list(...)
+        nm <- names(z)  # NULL if no additional arguments given
+        `f` <- function(x, val) { if (!(x %in% nm)) z[[x]] <<- val }
+        f('pch', 16)
+        f('mar', c(4, 4, 1, 0.5))
+        f('oma', c(0.1, 0.1, 0.1, 0.1))
+        f('bty', 'L')
+        f('pty', 's')
+        f('las', 1)
+        f('mgp', c(2, 0.4, 0))
+        f('tcl', -0.2)
+        if(!is.null(panels)) {
+                `auto_rowcol` <- function(n = panels) {
+                        if (n <= 3)
+                                c(1, n)
+                        else if (n <= 6)
+                                c(2, (n + 1)%/%2)
+                        else if (n <= 12)
+                                c(3, (n + 2)%/%3)
+                        else c(ceiling(n/(nr <- ceiling(sqrt(n)))), nr)
+                }
+                mfrow <- auto_rowcol()
+                f('mfrow', mfrow)
+                if (panels > 4) panels <- 4
+                switch(as.character(panels),
+                       `4` = f('cex.lab', 1.2),
+                       `3` = { f('cex.axis', 1.2); f('cex.lab', 1.4) },
+                       `2` = f('cex.axis', 0.85),
+                       `1` = f('cex.axis', 0.85))
+        }
+        if (length(names(z)) > 1) z <- z[order(names(z))]
+        invisible(par(z)) # set parameters, while keeping defaults
 }
 #' @export
 #' @rdname utils_plot
 `set_par_mercury` <- function(panels=NULL, CEX=1.0, ...) {
-     # if font not installed, then revert to `set_par()`
-     extrafont::loadfonts('win', quiet=TRUE)
-     if (!('Routed Gothic' %in% extrafont::fonttable()$FamilyName)) {
-          a <- c(as.list(environment()), list(...))
-          do.call(ecole::set_par, a)
-          warning('\nMust import/install/register the font from:\n
+        # if font not installed, then revert to `set_par()`
+        # extrafont::loadfonts('win', quiet=TRUE)
+        tryCatch({extrafont::loadfonts("pdf", quiet = TRUE)})
+        tryCatch({extrafont::loadfonts("postscript", quiet = TRUE)})
+        if (.Platform$OS.type == "windows") {
+                tryCatch({extrafont::loadfonts("win", quiet = TRUE)})
+        }
+        if (!('Routed Gothic' %in% extrafont::fonttable()$FamilyName)) {
+                a <- c(as.list(environment()), list(...))
+                do.call(ecole::set_par, a)
+                warning('\nMust import/install/register the font from:\n
                https://webonastick.com/fonts/routed-gothic/\n
           ...trying `ecole::set_par()` instead...')
-     }
-     # if font is successfully installed, then proceed
-     z  <- list(...)
-     nm <- names(z)  # NULL if no additional arguments given
-     `f` <- function(x, val) { if (!(x %in% nm)) z[[x]] <<- val }
-     f('cex.axis', CEX*1)
-     f('cex.lab',  CEX*1.3)
-     f('cex.main', CEX*1.3)
-     f('pch', 16)
-     f('mar', c(3, 3, 1, 0.5))
-     f('oma', c(0.1, 0.1, 0.1, 0.1))
-     f('family', 'Routed Gothic')
-     f('bty', 'L')
-     f('pty', 's')
-     f('las', 1)
-     f('mgp', c(CEX+0.5, 0.2, 0))
-     f('tcl', 0.2)
-     if(!is.null(panels)) {
-          `auto_rowcol` <- function(n = panels) {
-               if (n <= 3)
-                    c(1, n)
-               else if (n <= 6)
-                    c(2, (n + 1)%/%2)
-               else if (n <= 12)
-                    c(3, (n + 2)%/%3)
-               else c(ceiling(n/(nr <- ceiling(sqrt(n)))),nr)
-          }
-          f('mfrow', auto_rowcol())
-          if (panels > 4) panels <- 4
-          switch(as.character(panels),
-                 `4` = f('cex.lab',  CEX*1.4),
-                 `3` = {f('cex.axis', 1.0); f('cex.lab', 1.4)},
-                 `2` = f('cex.axis',  CEX*0.9),
-                 `1` = f('cex.axis',  CEX*0.9))
-          # TODO: mgp should also vary with number of panels...
-     }
-     if (length(names(z)) > 1) z <- z[order(names(z))]
-     invisible(par(z)) # set parameters, while keeping defaults
+        }
+        # if font is successfully installed, then proceed
+        z  <- list(...)
+        nm <- names(z)  # NULL if no additional arguments given
+        `f` <- function(x, val) { if (!(x %in% nm)) z[[x]] <<- val }
+        f('cex.axis', CEX*1)
+        f('cex.lab',  CEX*1.3)
+        f('cex.main', CEX*1.3)
+        f('pch', 16)
+        f('mar', c(3, 3, 1, 0.5))
+        f('oma', c(0.1, 0.1, 0.1, 0.1))
+        f('family', 'Routed Gothic')
+        f('bty', 'L')
+        f('pty', 's')
+        f('las', 1)
+        f('mgp', c(CEX+0.5, 0.2, 0))
+        f('tcl', 0.2)
+        if(!is.null(panels)) {
+                `auto_rowcol` <- function(n = panels) {
+                        if (n <= 3)
+                                c(1, n)
+                        else if (n <= 6)
+                                c(2, (n + 1)%/%2)
+                        else if (n <= 12)
+                                c(3, (n + 2)%/%3)
+                        else c(ceiling(n/(nr <- ceiling(sqrt(n)))),nr)
+                }
+                f('mfrow', auto_rowcol())
+                if (panels > 4) panels <- 4
+                switch(as.character(panels),
+                       `4` = f('cex.lab',  CEX*1.4),
+                       `3` = {f('cex.axis', 1.0); f('cex.lab', 1.4)},
+                       `2` = f('cex.axis',  CEX*0.9),
+                       `1` = f('cex.axis',  CEX*0.9))
+                # TODO: mgp should also vary with number of panels...
+        }
+        if (length(names(z)) > 1) z <- z[order(names(z))]
+        invisible(par(z)) # set parameters, while keeping defaults
 }
 #' @export
 #' @rdname utils_plot
 `add_label` <- function(labels, ...){
-     mtext(text=labels, side=3, adj=0, ...)
+        mtext(text=labels, side=3, adj=0, ...)
 }
 #' @export
 #' @rdname utils_plot
 `add_text` <- function(x, y, labels, ...){
-     text(x=graphics::grconvertX(x, from='npc', to='user'),
-          y=graphics::grconvertY(y, from='npc', to='user'),
-          labels=labels, adj=0, ...)
+        text(x=graphics::grconvertX(x, from='npc', to='user'),
+             y=graphics::grconvertY(y, from='npc', to='user'),
+             labels=labels, adj=0, ...)
 }
