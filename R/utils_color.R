b5bdafebedbd64b16407229ba6b7acc317d2b224
phytomosaic <smithr2@oregonstate.edu>
2020-09-15 06:07:14 -0700

    cleanup documentation

--- R/utils_color.R
+++ R/utils_color.R
@@ -4,21 +4,29 @@
 #' From given values, generate colors for plotting points or surfaces.
 #'
 #' @param x vector of values to evaluate
+#'
 #' @param n number of unique colors to be used in the palette
+#'
 #' @param alpha alpha transparency in [0,1]
+#'
 #' @param begin hue in [0,1] at which the viridis colormap begins
+#'
 #' @param end hue in [0,1] at which the viridis colormap ends
+#'
 #' @param dir order of colors in the scale; if 1, the default, colors
 #'  are ordered from darkest to lightest; if -1, the order of colors
 #'  is reversed
+#'
 #' @param pal optional specification for a color palette; defaults to
 #'  \code{viridis::inferno} palette if missing
+#'
 #' @param ... further arguments passed to other methods
+#'
 #' @param ngrid number of grid cells in the matrix of values plotted
-#' in \code{\link[graphics]{persp}}
+#'  in \code{\link[graphics]{persp}}
 #'
 #' @return
-#' Vector or matrix of color values
+#' Vector or matrix of color values.
 #'
 #' @examples
 #' # generate data
