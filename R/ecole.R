0edd3b64872cba2ac432f5048938c9fbf471207e
phytomosaic <smithr2@oregonstate.edu>
2021-12-14 06:09:42 -0800

    Update ecole-package man page

--- R/ecole.R
+++ R/ecole.R
@@ -5,42 +5,50 @@
 #'     in ecology and biogeography, including:
 #'
 #'     Basic functions for forestry and vegetation analysis:\cr
-#'     \code{\link{basal_area}}, \code{\link{p3_to_p2}},
-#'     \code{\link{foldasp}}, \code{\link{htld}}, \code{\link{pdir}},
-#'     \code{\link{nearestspecies}}
+#'     \code{\link{basal_area}}, \code{\link{foldasp}}, \code{\link{htld}},
+#'     \code{\link{pdir}}
 #'
 #'     Outlier analysis:\cr
 #'     \code{\link{outlier_multi}}, \code{\link{outlier_uni}}
 #'
-#'     Niche overlap:\cr
-#'     \code{\link{overlap}}
-#'
 #'     Plots:\cr
-#'     \code{\link{plot_heatmap}}, \code{\link{plot_joy}},
-#'     \code{\link{plot_loess}}, \code{\link{plot_marg}},
-#'     \code{\link{plot_ortho}}, \code{utils_*}
+#'     \code{\link{plot_heatmap}}, \code{\link{plot_loess}},
+#'     \code{\link{plot_lm}}, \code{utils_*}
 #'
 #'     Plot utilities:\cr
 #'      \code{\link{set_par}}, \code{\link{colvec}} and allies
 #'
-#'     Matrix manipulation and description:\cr
+#'     Text utilities:\cr
+#'      \code{\link{clean_text}}
+#'
+#'     Matrix manipulation and summaries:\cr
 #'     \code{\link{mx_collapse}}, \code{\link{mx_diversity}},
+#'     \code{\link{mx_profile}}, \code{\link{mx_describe}},
 #'     \code{\link{mx_dropzero}}, \code{\link{mx_rbind_all}},
-#'     \code{\link{mx_valid}}, \code{\link{reshape_w}},
-#'     \code{\link{describe}}
+#'     \code{\link{mx_valid}}, \code{\link{reshape_w}}
 #'
 #'     Data transformations:\cr
 #'     \code{\link{asin_sqrt}}, \code{\link{genlogtrans}},
-#'     \code{\link{standardize}}'
+#'     \code{\link{pseudo_log}}, \code{\link{standardize}}
 #'
 #'     Basic statistics:\cr
-#'     \code{\link{rmse}} and allies
+#'     \code{\link{cv}}, \code{\link{sem}}, \code{\link{rmse}} and allies
 #'
-#'     Handling text and character strings:\cr
-#'      \code{\link{clean_text}}
+#'     Ordination methods (reluctantly):\cr
+#'     \code{\link{ord_bc}}, \code{\link{ord_nms}}, \code{\link{ord_pca}}
+#'
+#'     Pairwise multiple comparisons for PERMANOVA:\cr
+#'     \code{\link{permanova_pairwise}}
+#'
+#'     Blocked indicator species analysis:\cr
+#'     \code{\link{indval}}
+#'
+#'     Niche overlap:\cr
+#'     \code{\link{overlap}}
 #'
 #'     Datasets:\cr
-#'     \code{\link{smoky}}, \code{\link{braun}}
+#'     \code{\link{smoky}}, \code{\link{braun}}, \code{\link{veg}},
+#'     \code{\link{foods}}, \code{\link{bison}}, \code{\link{oakwoods}}
 #'
 #'     ...and many others.
 #'
